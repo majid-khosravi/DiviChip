e86fd104535a5593c5efa9aa8cc78a50c7fdbc44
majid <majid.khosravi89@gmail.com>
2023-11-15 13:14:03 +0330

    All changes

--- app/src/main/java/com/chips/divisive/MainActivity.kt
+++ app/src/main/java/com/chips/divisive/MainActivity.kt
@@ -7,12 +7,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -20,11 +20,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -36,6 +40,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -45,6 +50,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.chips.divisive.ui.theme.ChipsDivisiveTheme
@@ -67,10 +73,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+private val RED_CHIPS_COUNT = 38
+private val BLUE_CHIPS_COUNT = 68
+private val GREEN_CHIPS_COUNT = 102
+private val YELLOW_CHIPS_COUNT = 33
+
 @Composable
 fun MainScreen() {
     Box(
-        modifier = with(Modifier) {
+        modifier = with(Modifier.fillMaxWidth()) {
             fillMaxSize()
                 .paint(
                     painterResource(id = R.drawable.bg_home),
@@ -80,6 +92,9 @@ fun MainScreen() {
         Column(
             modifier = Modifier
                 .padding(16.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            Arrangement.SpaceBetween
         ) {
             val playersCount = remember {
                 mutableStateOf(1)
@@ -93,12 +108,19 @@ fun MainScreen() {
 //            ChipsRow(playersCount.value)
 
 
-            Row(modifier = Modifier.fillMaxWidth(), Arrangement.Center) {
+            Column(
+                modifier = Modifier.fillMaxWidth()
+                    .weight(weight =1f, fill = true),
+                Arrangement.Bottom,
+                Alignment.CenterHorizontally
+            ) {
                 Text(
-                    text = "Developed with <3 in the Island", modifier = Modifier
+                    text = "Developed with ❤️ in Jazireh \uD83C\uDFDD️", modifier = Modifier
                         .fillMaxWidth(),
                     color = Color.Black,
-                    fontStyle = FontStyle.Italic
+                    fontStyle = FontStyle.Normal, style = TextStyle(
+                        textAlign = TextAlign.Center
+                    )
                 )
             }
         }
@@ -110,39 +132,61 @@ fun MainScreen() {
 fun ChipsTable(playersCount: Int) {
 
     Card(
-        border = BorderStroke(1.dp, Color.White),
+        border = BorderStroke(4.dp, Color.Black),
         modifier = Modifier
             .padding(16.dp)
             .fillMaxWidth()
     ) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .verticalScroll(rememberScrollState())
+            .weight(weight =1f, fill = false)) {
 
-        Column(modifier = Modifier.fillMaxWidth()) {
+//            .verticalScroll(rememberScrollState())
+            ChipListItems(playersCount)
+//            ChipRows(playersCount)
 
-
-            val red = 38.div(playersCount)
-            val blue = 67.div(playersCount)
-            val green = 100.div(playersCount)
-            val yellow = 33.floorDiv(playersCount)
-
-            val redRemained = 38 % (playersCount)
-            val blueRemained = 67 % (playersCount)
-            val greenRemained = 100 % (playersCount)
-            val yellowRemained = 33 % (playersCount)
-
-            ChipRow("100", Color.Red, Color.White, red, redRemained)
-            Divider(color = Color.White, thickness = 1.dp)
-
-            ChipRow("50", Color.Blue, Color.White, blue, blueRemained)
-            Divider(color = Color.White, thickness = 1.dp)
-
-            ChipRow("25", Color.Green, Color.Black, green, greenRemained)
-            Divider(color = Color.White, thickness = 1.dp)
-
-            ChipRow("10", Color.Yellow, Color.Black, yellow, yellowRemained)
-
+//
         }
     }
 
+}
+
+@Composable
+fun ChipRows(playersCount: Int) {
+
+    val red = RED_CHIPS_COUNT.div(playersCount)
+    val blue = BLUE_CHIPS_COUNT.div(playersCount)
+    val green = GREEN_CHIPS_COUNT.div(playersCount)
+    val yellow = YELLOW_CHIPS_COUNT.floorDiv(playersCount)
+
+    val redRemained = RED_CHIPS_COUNT % (playersCount)
+    val blueRemained = BLUE_CHIPS_COUNT % (playersCount)
+    val greenRemained = GREEN_CHIPS_COUNT % (playersCount)
+    val yellowRemained = YELLOW_CHIPS_COUNT % (playersCount)
+    ChipRow("100$", Color.Red, Color.White, red, redRemained)
+    ChipRow("50 $", Color.Blue, Color.White, blue, blueRemained)
+    ChipRow("25 $", Color.Green, Color.Black, green, greenRemained)
+    ChipRow("10 $", Color.Yellow, Color.Black, yellow, yellowRemained)
+
+}
+
+@Composable
+fun ChipListItems(playersCount: Int) {
+
+    val red = RED_CHIPS_COUNT.div(playersCount)
+    val blue = BLUE_CHIPS_COUNT.div(playersCount)
+    val green = GREEN_CHIPS_COUNT.div(playersCount)
+    val yellow = YELLOW_CHIPS_COUNT.floorDiv(playersCount)
+
+    val redRemained = RED_CHIPS_COUNT % (playersCount)
+    val blueRemained = BLUE_CHIPS_COUNT % (playersCount)
+    val greenRemained = GREEN_CHIPS_COUNT % (playersCount)
+    val yellowRemained = YELLOW_CHIPS_COUNT % (playersCount)
+    ChipListItem("100$", Color.Red, Color.White, red, redRemained)
+    ChipListItem("50 $", Color.Blue, Color.White, blue, blueRemained)
+    ChipListItem("25 $", Color.Green, Color.Black, green, greenRemained)
+    ChipListItem("10 $", Color.Yellow, Color.Black, yellow, yellowRemained)
 }
 
 @Composable
@@ -176,21 +220,21 @@ fun ChipsRow(playersCount: Int) {
 @Composable
 fun ButtonsCard(callback: (Int) -> Unit) {
     Card(
-        border = BorderStroke(2.dp, Color.White),
-        shape = RoundedCornerShape(8.dp)
+        border = BorderStroke(4.dp, Color.Black),
+        shape = RoundedCornerShape(8.dp),
+        modifier = Modifier.padding(16.dp),
     ) {
         Column(
             modifier = Modifier
-                .padding(16.dp)
+                .padding(8.dp)
                 .fillMaxWidth(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-//                text = "تعداد مورد نظر را انتخاب کنید",
-                text = "How many guys want to play? ",
+                text = "How many guys want to play?",
                 modifier = Modifier
-                    .padding(16.dp)
+                    .padding(8.dp)
                     .fillMaxWidth()
                     .align(Alignment.CenterHorizontally),
                 color = Color.White,
@@ -268,6 +312,37 @@ fun Chip(
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ChipListItem(
+    value: String,
+    color: Color,
+    textColor: Color,
+    count: Int,
+    remained: Int
+) {
+    Column {
+        ListItem(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .background(Color.White),
+            headlineText = { Text(text = "$count each person") },
+            supportingText = { Text(text = "$remained chips are remained") },
+            colors = ListItemDefaults.colors(containerColor = Color(0xFF45464f)),
+            leadingContent = {
+                CircleShape(
+                    text = value,
+                    color = color,
+                    textColor = textColor
+                )
+            },
+        )
+        Divider(color = Color.Black, thickness = 3.dp)
+    }
+}
+
+
 @Composable
 fun ChipRow(
     value: String,
@@ -276,70 +351,38 @@ fun ChipRow(
     count: Int,
     remained: Int
 ) {
+    Column {
 
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
 
-        CircleShape(color, textColor, value)
-        Column(modifier = Modifier.widthIn(min = Dp.Unspecified, max = Dp.Infinity)) {
-            Text(
-                text = "$count each one", Modifier.padding(8.dp),
-                style = TextStyle(
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center,
-                    fontSize = 17.sp
-                ),
-                textAlign = TextAlign.Center,
-            )
-            Text(
-                text = "Remained: $remained", Modifier.padding(8.dp),
-                style = TextStyle(
-                    color = Color.LightGray,
-                    textAlign = TextAlign.Center,
-                    fontSize = 13.sp
-                ),
-                textAlign = TextAlign.Center,
-            )
-        }
-
-
-//            ListItem()
-
-        /*    ListItem(
-                text = { Text("Two line list item with 40x40 icon") },
-                secondaryText = { Text("Secondary text") },
-                icon = {
-                    Icon(
-                        Icons.Filled.Favorite,
-                        contentDescription = null,
-                        modifier = Modifier.size(40.dp)
-                    )
-                }
-            )*/
-        /*            Text(
-                        modifier = Modifier
-                            .background(color, shape = RoundedCornerShape(36.dp))
-                            .width(72.dp)
-                            .height(72.dp)
-                            .padding(8.dp)
-                            .padding(8.dp),
-                        text = value,
-                        style = TextStyle(
-                            color = textColor,
-                            fontWeight = FontWeight.Bold,
-                            textAlign = TextAlign.Center,
-                            fontSize = 15.sp
-                        ),
+            CircleShape(color, textColor, value)
+            Column(modifier = Modifier.widthIn(min = Dp.Unspecified, max = Dp.Infinity)) {
+                Text(
+                    text = "$count each person", Modifier.padding(8.dp),
+                    style = TextStyle(
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold,
                         textAlign = TextAlign.Center,
-                    )*/
-
-//            Text(text = "$remained")
-
-
+                        fontSize = 17.sp
+                    ),
+                    textAlign = TextAlign.Center,
+                )
+                Text(
+                    text = "$remained chips are remained", Modifier.padding(8.dp),
+                    style = TextStyle(
+                        color = Color.LightGray,
+                        textAlign = TextAlign.Center,
+                        fontSize = 13.sp
+                    ),
+                    textAlign = TextAlign.Center,
+                )
+            }
+        }
+        Divider(color = Color.Black, thickness = 3.dp)
     }
 }
 
@@ -353,7 +396,8 @@ fun ConstraintLayoutContent() {
 @Composable
 fun CircleShape(
     color: Color,
-    textColor: Color, text: String
+    textColor: Color, text: String,
+    fontSize: TextUnit = 15.sp
 ) {
     Box(modifier = Modifier.padding(8.dp)) {
         Canvas(modifier = Modifier.size(50.dp), onDraw = {
@@ -372,7 +416,39 @@ fun CircleShape(
                 color = textColor,
                 fontWeight = FontWeight.Bold,
                 textAlign = TextAlign.Center,
-                fontSize = 15.sp
+                fontSize = fontSize
+            )
+        )
+    }
+}
+
+@Composable
+fun RoundedShape(
+    color: Color,
+    textColor: Color, text: String,
+    fontSize: TextUnit = 15.sp,
+    callback: (() -> Unit)? = null
+) {
+    Box(modifier = Modifier.padding(8.dp)) {
+        Canvas(modifier = Modifier.size(50.dp), onDraw = {
+            val size = 42.dp.value
+            drawRoundRect(
+                color = color,
+                cornerRadius = CornerRadius(size, size)
+            )
+        })
+        Text(
+            text = text,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .clickable {
+                    callback?.let { it() }
+                },
+            style = TextStyle(
+                color = textColor,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                fontSize = fontSize
             )
         )
     }
@@ -429,44 +505,32 @@ fun ControllerButtons(callback: (Int) -> Unit) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp),
+            .padding(0.dp),
         Arrangement.Center,
         Alignment.CenterVertically
     ) {
-        Text(
-            text = "-",
-            Modifier
-                .width(70.dp)
-                .clip(RoundedCornerShape(8.dp))
-                .clickable {
-                    if (text.value > 1) {
-                        text.value--
-                        callback(text.value)
-                    }
-                }
-                .border(2.dp, Color.Black)
-                .size(42.dp)
-                .background(Color.Red)
-                .height(70.dp),
-            color = Color.Black,
-            fontSize = 32.sp,
-            textAlign = TextAlign.Center,
+
+        ControlsButton(text = "-", color = Color(0xFFFF2011)) {
+            if (text.value > 1) {
+                text.value--
+                callback(text.value)
+            }
+        }
+
+        RoundedShape(
+            color = Color.White,
+            textColor = Color.Black, text = text.value.toString(), fontSize = 32.sp
         )
 
-        Text(
-            text = text.value.toString(),
-            modifier = Modifier
-                .width(70.dp)
-                .height(70.dp)
-                .size(42.dp)
-                .background(Color.White)
-                .align(Alignment.CenterVertically)
-                .height(70.dp),
-            color = Color.Black,
-            fontSize = 34.sp,
-            textAlign = TextAlign.Center,
-        )
-        Text(
+        ControlsButton(text = "+", color = Color(0x9900FF22)) {
+            if (text.value < 10) {
+                text.value++
+                callback(text.value)
+            }
+        }
+
+
+        /*Text(
             text = "+",
             Modifier
                 .clickable {
@@ -483,9 +547,45 @@ fun ControllerButtons(callback: (Int) -> Unit) {
             color = Color.Black,
             fontSize = 32.sp,
             textAlign = TextAlign.Center,
-        )
+        )*/
     }
 
+}
+
+
+@Composable
+fun ControlsButton(text: String, color: Color, callback: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .padding(8.dp)
+            .clip(shape = RoundedCornerShape(8.dp)),
+        contentAlignment = Alignment.CenterStart,
+    ) {
+        RoundedShape(
+            color = color,
+            textColor = Color.White,
+            text = text,
+            fontSize = 32.sp,
+            callback = callback
+        )
+
+        /*
+        Text(
+            text = text,
+            Modifier
+                .clickable {
+                    callback()
+                }
+                .border(2.dp, Color.Black)
+                .width(70.dp)
+                .background(color)
+                .size(42.dp)
+                .height(70.dp),
+            color = Color.Black,
+            fontSize = 32.sp,
+            textAlign = TextAlign.Center,
+        )*/
+    }
 }
 
 
